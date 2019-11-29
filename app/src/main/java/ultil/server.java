e54157121898f2854f916ed1311fe02e1a217019
baphunguyen <58042966+baphunguyen@users.noreply.github.com>
2019-11-29 17:16:57 +0700

    Hotel Booking App

---
+++ app/src/main/java/ultil/server.java
@@ -0,0 +1,17 @@
+package ultil;
+
+public class server {
+    public static String localhost = "datphongkhachsanonline.000webhostapp.com/";
+
+    public static String duongdan_loaisp = "http://" + localhost + "/server/getloaisp.php";
+    public static String duongdan_spmoinhat = "http://" + localhost + "/server/getspmoinhat.php";
+    public static String duongdan_khachsan = "http://" + localhost + "/server/getsp.php?page=";
+    public static String duongdan_donhang = "http://" + localhost + "/server/thongtinkhachhang.php";
+    public static String duongdan_chitietdonhang = "http://" + localhost + "/server/chitietdonhang.php";
+    public static String duongdan_loaiphong = "http://" + localhost + "/server/getloaiphong.php";
+    public static String duongdan_taikhoan = "http://" + localhost + "/server/gettaikhoan.php";
+    public static String duongdan_inserttaikhoan = "http://" + localhost + "/server/insertTaiKhoan.php";
+    public static String duongdan_updatematkhau = "http://" + localhost + "/server/updateMatKhau.php";
+    public static String duongdan_updatethongtin = "http://" + localhost + "/server/updateThongTinTK.php";
+    public static String duongdan_khachsandadat = "http://" + localhost + "/server/getkhachsandadat.php";
+}
